9f0625cc1e03791313a9df25e4cf582624a38b95
Marko Pantovic <leotar@gmail.com>
2024-11-23 17:21:59 +0100

    Init commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync"
+)
+
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go receiveOrders(&wg)
+	wg.Wait()
+	fmt.Println(orders)
+}
+
+func receiveOrders(wg *sync.WaitGroup) {
+	for _, rawOrder := range rawOrders {
+		var newOrder order
+		err := json.Unmarshal([]byte(rawOrder), &newOrder)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		orders = append(orders, newOrder)
+	}
+	wg.Done()
+}
+
+var rawOrders = []string{
+	`{"productCode": 1111, "quantity": 5, "status": 1}`,
+	`{"productCode": 2222, "quantity": 42.3, "status": 1}`,
+	`{"productCode": 3333, "quantity": 19, "status": 1}`,
+	`{"productCode": 4444, "quantity": 8, "status": 1}`,
+}
